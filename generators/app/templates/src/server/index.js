051a1918b4ef518da011dd7bae56d0e1b7bcfb97
Andrew McGill <arm5077@gmail.com>
2019-06-02 17:06:52 -0400

    Further updates for server production build

--- generators/app/templates/src/server/index.js
+++ generators/app/templates/src/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('client'));
 app.listen(process.env.PORT || 61386, () => console.log(`Listening on port ${process.env.PORT || 61386}!`));
